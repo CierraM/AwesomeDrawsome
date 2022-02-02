0163e8ff4f1ffeb5549f8f94bbff5f0273eddcb9
Cierra Morris <4cibe@MacBook-Pro-10.localdomain>
2022-02-02 16:25:12 -0700

    added color picker

---
+++ src/components/toolbar/controls/ToolBarControl.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+    IconButton,
+    Menu,
+    MenuButton,
+    MenuList,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+    PopoverCloseButton
+} from "@chakra-ui/react"
+
+
+const ToolBarControl = props => {
+    console.log(props.color)
+    return (
+        <Popover position="top-start">
+            <PopoverTrigger
+            >
+                <IconButton
+                    m={1}
+                    icon={props.icon}
+                    isRound="true"
+                    fontSize="24px"
+                    size="lg"
+                    variant={props.variant || "ghost"}
+                    color={props.color || "teal.200"}
+                    _hover={{ color: props.color || "teal.500", bg: "white" }}
+                    _focus={{ outline: 0}}
+                />
+            </PopoverTrigger>
+            <PopoverContent w="auto" border="none" _focus={{ outline: 0}}>
+            <PopoverCloseButton />
+                {props.children}
+            </PopoverContent>
+        </Popover>
+    )
+}
+
+export default ToolBarControl;
