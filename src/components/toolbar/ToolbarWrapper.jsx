5251d29562982c232de2bfee44c07bf4b9ef23c8
Cierra Morris <4cibe@MacBook-Pro-10.localdomain>
2022-02-04 10:16:09 -0700

    added undo/redo icons

--- src/components/toolbar/ToolbarWrapper.jsx
+++ src/components/toolbar/ToolbarWrapper.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {
-    Box, Image, Flex, Spacer
+    Box, Image, Flex, Spacer, Tooltip
 } from "@chakra-ui/react";
 
 import { FaPaintBrush, FaEraser, FaSave } from 'react-icons/fa'
 import { VscCircleLargeFilled } from 'react-icons/vsc'
+import { IoArrowUndoSharp, IoArrowRedoSharp } from 'react-icons/io5'
 
 import ToolBarControl from "./controls/ToolBarControl";
 import ColorPicker from "./controls/ColorPicker"
@@ -20,12 +21,20 @@ const ToolbarWrapper = (props) => {
     return (
         <Flex w="100%" h="80px" bg="#212121" p="10px" position='fixed' bottom='0px' align="center">
             <Box mr="30px">
-                <BrushComponent icon={<FaPaintBrush />} variant={isErasing? 'ghost': 'solid' }/>
-                <EraserComponent icon={<FaEraser />} variant={isErasing ? 'solid' : 'ghost'} color={isErasing && 'grey'}/>
+                <BrushComponent icon={<FaPaintBrush />} variant={isErasing ? 'ghost' : 'solid'} />
+                <EraserComponent icon={<FaEraser />} variant={isErasing ? 'solid' : 'ghost'} color={isErasing && 'grey'} />
             </Box>
             <Spacer></Spacer>
-            <Image src={logo} draggable="false" alt="logo" height="30px" ml="-30px"></Image>
+            <Image src={logo} draggable="false" alt="logo" height="30px"></Image>
             <Spacer></Spacer>
+            <Box mr="20px">
+                <Tooltip label="Undo">
+                    <ToolBarControl icon={<IoArrowUndoSharp />} />
+                </Tooltip>
+                <Tooltip label="redo">
+                    <ToolBarControl icon={<IoArrowRedoSharp />} />
+                </Tooltip>
+            </Box>
             <ToolBarControl icon={<FaSave />} />
         </Flex>
     )
--- src/components/toolbar/controls/ToolBarControl.jsx
+++ src/components/toolbar/controls/ToolBarControl.jsx
@@ -8,7 +8,7 @@ import {
 } from "@chakra-ui/react"
 
 
-const ToolBarControl = props => {
+const ToolBarControl = (props) => {
     return (
         <Popover position="top-start" {...props }>
             <PopoverTrigger
